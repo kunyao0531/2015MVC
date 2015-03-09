b87d04f22ef62fef0575b94cf7e51f4c898e4380
kunyao0531 <frankwang11@hotmail.com>
2015-03-09 14:05:57 +0800

    Frank update

---
+++ 2015MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_2015MVC.Startup))]
+namespace _2015MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
